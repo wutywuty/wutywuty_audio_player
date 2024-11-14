14856c850b051749ecfe47374465548793dafdaa
wutywuty <tsk007sk@gmail.com>
2024-11-14 16:16:55 +1300

    changed some stuff to make it work on windows properly (pray it still works on linux)

--- script/file_handler.gd
+++ script/file_handler.gd
@@ -7,19 +7,22 @@ func _ready() -> void:
 
 func save_playlist(dir):
 	# gets the name of the directory (is there a better way??)
-	var playlist_name = dir.split("/")[len(dir.split("/"))-1]
+	var dir_fixed = dir.replace("\\", "/")
+	var playlist_name = dir_fixed.split("/")[len(dir_fixed.split("/"))-1]
 	var file = FileAccess.open("user://"+playlist_name, FileAccess.WRITE_READ)
-	var playlist_dir = DirAccess.open(dir)
+	var playlist_dir = DirAccess.open(dir_fixed)
 	var songs = playlist_dir.get_files()
+	print(songs)
 	var song_data = []
 	var index = 0
 	for song in songs:
 		if not song.ends_with("mp3"):
 			continue
-		song_data.append({"index":index, "name":song,"path":dir+"/"+song})
+		song_data.append({"index":index, "name":song,"path":dir_fixed+"/"+song})
 		index += 1
-	var playlist_data = {"name":playlist_name,"path":dir,"song_data":song_data}
+	var playlist_data = {"name":playlist_name,"path":dir_fixed,"song_data":song_data}
 	file.store_var(playlist_data)
+	print(playlist_data)
 	file.close()
 	Signals.refresh_playlists.emit()
 	
